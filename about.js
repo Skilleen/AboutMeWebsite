b83e1dbfa63f2df446ca05f39bd696b037fa0d71
Scott Killeen <scott.killeen@hotmail.com>
2015-02-08 20:42:03 -0500

    JS

---
+++ about.js
@@ -0,0 +1,37 @@
+function education () {
+	var div = document.getElementById("mainArea").innerHTML = "Scott Killeen is currently a 2nd year Computing student at Queens, his previous education includes:" 
+	+ "<ul> <li>Stittsville Public School: Kindergarten - Grade 5</li>" 
+	+ "<li> Goulbourn Middle School: Grade 6 - grade 8 </li>" 
+	+ "<li> Sacred Heart Catholic High School: Grade 9 - grade 12 </li> </ul>"
+	+ "Scott plans to do an internship with IBM between his 3rd and 4th year. His specialization is in software design. <br><br><br>"
+	+ "<img src='degree.jpg'/>";
+}
+function interests(){
+	var div = document.getElementById("mainArea").innerHTML = "Scott Killeen has several hobbies and interests:"
+	+ "<ul> <li>Hockey (Go Bruins Go!)</li>" 
+	+ "<li> Computers</li>" 
+	+ "<li> Video games</li>" 
+	+ "<li> Being in da club</li>"
+	+ "<li> Programming (Favourite language is Java)</li>" 
+	+ "<li> Travelling to some place hot</li>"
+	+ "<li> Painting</li>" 
+	+ "<li> Music (Classic Rock)</li>"
+	+ "<li> Watching Netflix </ul>"
+	+ "<img src='computer.jpg'/>";
+
+}
+
+function thehat(){
+	var div = document.getElementById("mainArea").innerHTML = "<p>No matter where Scott is or what time it is, Scott can always be found in his white hat."
+	+ " In fact, more people know Scott as the dude in the white hat rather than Scott himself. So why the white hat you might ask? Well here is some reasons why: <p>"
+	+ "<ul> <li>Gangster rap made me do it</li>" 
+	+ "<li> Needed some form of identity</li>" 
+	+ "<li> A way girls can remember me by</li>" 
+	+ "<li> Following in 50 cents foot steps</li>"
+	+ "<li> Fits snug on my head</li>" 
+	+ "<li> It was love at first sight</li></ul>"
+	+ "Scott's hat is an important part of his life, so much so that he has bought the same one three times now.Where ever he goes, his hat goes with him.<br><br><br>"
+	+ "<img src='hatone.jpg'/>"
+	+ "<img src='hattwo.jpg'/>"
+	+ "<img src='hatthree.jpg'/>";
+}
